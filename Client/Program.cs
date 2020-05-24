ffdf406e6d9fc1783df1a61ec2b24f2827751bf8
Xeevis <Xeevis@users.noreply.github.com>
2020-05-24 23:20:56 +0200

    Init

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bulma;
+
+namespace BlazorApp1.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = true;
+            }).AddBulmaProviders();
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            var host = builder.Build();
+
+            host.Services.UseBulmaProviders();
+
+            await host.RunAsync();
+        }
+    }
+}
